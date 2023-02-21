8593fb8a7aab66f17839a8e3a9fab68ea7bcfe36
you min <sims2245@naver.com>
2023-02-21 18:18:44 +0900

    9days

---
+++ javascript/20230220/id.js
@@ -0,0 +1,3 @@
+var id,password;
+id=prompt('아이디 입력');
+if(id=='admin'){password=prompt('비밀번호 입력');if(location.href="http://www.naver.com")else(location.href="http://www.youtube.com")}}else{location.href("http://wwww.daum.net")};
